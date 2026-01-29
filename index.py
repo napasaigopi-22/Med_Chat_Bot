489334fabc71cc809d79366c4557905a9324ee26
unknown <cb.sc.p2cse24008@cb.students.amrita.edu>
2026-01-29 20:20:41 +0530

    genai

--- index.py
+++
@@ -1,6 +0,0 @@
-print("hello Med_chat_bot")
-print("hello Med_chat_bot")
-print("hello Med_chat_bot")
-print("hello Med_chat_bot")
-print("hello Med_chat_bot")
-print("hello Med_chat_bot")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generative AI Agent',
+    version = '0.1.0',
+    author='NAPA SAI GOPI',
+    author_email="cb.sc.p2cse24008@cb.students.amrita.edu",
+    description="A Generative AI Agent using Langchain and Flask",
+    packages= find_packages(),
+    install_requires=[]
+
+)
